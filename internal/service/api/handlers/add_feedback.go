2d5f8af3c1f76cb67f779b5d2278942d4c26265d
Maksym Hrynenko <mg06062003@gmail.com>
2023-08-10 13:58:16 +0300

    update feedbackRegistry contract, add check for feedback length content

--- internal/service/api/handlers/add_feedback.go
+++ internal/service/api/handlers/add_feedback.go
@@ -1,12 +1,15 @@
 package handlers
 
 import (
+	"context"
+	"math/big"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/dov-id/publisher-svc/internal/config"
 	"github.com/dov-id/publisher-svc/internal/data"
+	"github.com/dov-id/publisher-svc/internal/helpers"
 	"github.com/dov-id/publisher-svc/internal/service/api/requests"
 	"github.com/dov-id/publisher-svc/internal/service/api/responses"
 	"github.com/dov-id/publisher-svc/resources"
@@ -15,6 +18,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/iden3/go-rapidsnark/types"
 	"github.com/iden3/go-rapidsnark/verifier"
+	pkgErrors "github.com/pkg/errors"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
 	"gitlab.com/distributed_lab/logan/v3"
@@ -46,24 +50,25 @@ func AddFeedback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//err = sendFeedbackToContract(ParentCtx(r), request, addFeedbackParams{
-	//	request:   dbRequest,
-	//	requestsQ: RequestsQ(r),
-	//	logger:    Log(r),
-	//	wallet:    Cfg(r).Networks().Networks[data.Network(request.Data.Attributes.Network)].WalletCfg,
-	//})
-	//if err != nil {
-	//	dbRequest.Error = err.Error()
-	//	dbRequest.Status = data.RequestsStatusFailed
-	//	err = RequestsQ(r).Update(dbRequest)
-	//	if err != nil {
-	//		err = errors.Wrap(err, "failed to update request status")
-	//	}
-	//
-	//	Log(r).WithError(err).Debugf("failed to send feedback to contract")
-	//	ape.RenderErr(w, problems.InternalError())
-	//	return
-	//}
+	err = sendFeedbackToContract(ParentCtx(r), request, addFeedbackParams{
+		request:   dbRequest,
+		requestsQ: RequestsQ(r),
+		logger:    Log(r),
+		wallet:    Cfg(r).Networks().Networks[data.Network(request.Data.Attributes.Network)].WalletCfg,
+	})
+	if err != nil {
+		Log(r).WithError(err).Debugf("failed to send feedback to contract")
+
+		dbRequest.Error = err.Error()
+		dbRequest.Status = data.RequestsStatusFailed
+		err = RequestsQ(r).Update(dbRequest)
+		if err != nil {
+			err = errors.Wrap(err, "failed to update request status")
+		}
+
+		ape.RenderErr(w, problems.InternalError())
+		return
+	}
 
 	w.WriteHeader(http.StatusAccepted)
 	ape.Render(w, responses.NewRequestResponse(dbRequest))
@@ -121,89 +126,130 @@ type addFeedbackParams struct {
 	logger    *logan.Entry
 	wallet    *config.WalletCfg
 
-	auth     *bind.TransactOpts
-	course   common.Address
-	ipfsHash string
+	auth        *bind.TransactOpts
+	course      common.Address
+	ipfsHash    string
+	pA_         [2]*big.Int
+	pB_         [2][2]*big.Int
+	pC_         [2]*big.Int
+	pubSignals_ [11]*big.Int
 }
 
-//
-//func sendFeedbackToContract(ctx context.Context, req requests.AddFeedbackRequest, params addFeedbackParams) error {
-//	network := data.Network(req.Data.Attributes.Network)
-//
-//	clients, err := helpers.GetNetworkClientsFromCtx(ctx)
-//	if err != nil {
-//		return errors.Wrap(err, "failed to get network clients from context")
-//	}
-//
-//	auth, err := helpers.GetAuth(ctx, clients[network], params.wallet)
-//	if err != nil {
-//		return errors.Wrap(err, "failed to get auth options")
-//	}
-//
-//	err = prepareAddFeedbackParams(req, &params)
-//	if err != nil {
-//		return errors.Wrap(err, "failed to prepare add feedback params")
-//	}
-//
-//	params.network = network
-//	params.auth = auth
-//
-//	err = makeAddFeedbackTx(ctx, params)
-//	if err != nil {
-//		return errors.Wrap(err, "failed to make add feedback transaction")
-//	}
-//
-//	return nil
-//}
-//
-//func makeAddFeedbackTx(ctx context.Context, params addFeedbackParams) error {
-//	feedbackRegistries, err := helpers.GetFeedbackRegistriesFromCtx(ctx)
-//	if err != nil {
-//		return errors.Wrap(err, "failed to get feedback registries from context")
-//	}
-//
-//	clients, err := helpers.GetNetworkClientsFromCtx(ctx)
-//	if err != nil {
-//		return errors.Wrap(err, "failed to get network clients from context")
-//	}
-//
-//	transaction, err := feedbackRegistries[params.network].AddFeedback(
-//		params.auth,
-//		params.course,
-//		params.i,
-//		params.c,
-//		params.r,
-//		params.publicKeysX,
-//		params.publicKeysY,
-//		params.merkleTreeProofs,
-//		params.keys,
-//		params.values,
-//		params.ipfsHash,
-//	)
-//
-//	if err != nil {
-//		if pkgErrors.Is(err, data.ErrReplacementTxUnderpriced) {
-//			params.auth.Nonce = big.NewInt(params.auth.Nonce.Int64() + 1)
-//			return makeAddFeedbackTx(ctx, params)
-//		}
-//
-//		return errors.Wrap(err, "failed to add feedback")
-//	}
-//
-//	params.request.Status = data.RequestsStatusInProgress
-//	err = params.requestsQ.Update(params.request)
-//	if err != nil {
-//		return errors.Wrap(err, "failed to update request status")
-//	}
-//
-//	helpers.WaitForTransactionMined(
-//		ctx,
-//		clients[params.network],
-//		transaction,
-//		params.logger,
-//		params.request,
-//		params.requestsQ,
-//	)
-//
-//	return nil
-//}
+func sendFeedbackToContract(ctx context.Context, req requests.AddFeedbackRequest, params addFeedbackParams) error {
+	network := data.Network(req.Data.Attributes.Network)
+
+	clients, err := helpers.GetNetworkClientsFromCtx(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get network clients from context")
+	}
+
+	auth, err := helpers.GetAuth(ctx, clients[network], params.wallet)
+	if err != nil {
+		return errors.Wrap(err, "failed to get auth options")
+	}
+
+	err = prepareAddFeedbackParams(req, &params)
+	if err != nil {
+		return errors.Wrap(err, "failed to prepare add feedback params")
+	}
+
+	params.network = network
+	params.auth = auth
+
+	err = makeAddFeedbackTx(ctx, params)
+	if err != nil {
+		return errors.Wrap(err, "failed to make add feedback transaction")
+	}
+
+	return nil
+}
+
+func prepareAddFeedbackParams(
+	req requests.AddFeedbackRequest,
+	params *addFeedbackParams,
+) error {
+	var ok bool
+
+	for i := 0; i < 2; i++ {
+		params.pA_[i], ok = new(big.Int).SetString(req.Data.Attributes.ZkProof.Proof.PiA[i], 10)
+		if !ok {
+			return data.ErrFailedToSetString
+		}
+
+		j := 0
+		l := 1
+		for ; j < 2; j++ {
+			params.pB_[i][j], ok = new(big.Int).SetString(req.Data.Attributes.ZkProof.Proof.PiB[i][l], 10)
+			if !ok {
+				return data.ErrFailedToSetString
+			}
+			l--
+		}
+
+		params.pC_[i], ok = new(big.Int).SetString(req.Data.Attributes.ZkProof.Proof.PiC[i], 10)
+		if !ok {
+			return data.ErrFailedToSetString
+		}
+	}
+
+	for i := 0; i < 11; i++ {
+		params.pubSignals_[i], ok = new(big.Int).SetString(req.Data.Attributes.ZkProof.PubSignals[i], 10)
+		if !ok {
+			return data.ErrFailedToSetString
+		}
+	}
+	
+	params.course = common.HexToAddress(req.Data.Attributes.Course)
+
+	params.ipfsHash = req.Data.Attributes.Feedback
+
+	return nil
+}
+
+func makeAddFeedbackTx(ctx context.Context, params addFeedbackParams) error {
+	feedbackRegistries, err := helpers.GetFeedbackRegistriesFromCtx(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get feedback registries from context")
+	}
+
+	clients, err := helpers.GetNetworkClientsFromCtx(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get network clients from context")
+	}
+
+	transaction, err := feedbackRegistries[params.network].AddFeedback(
+		params.auth,
+		params.course,
+		params.ipfsHash,
+		params.pA_,
+		params.pB_,
+		params.pC_,
+		params.pubSignals_,
+	)
+
+	if err != nil {
+		if pkgErrors.Is(err, data.ErrReplacementTxUnderpriced) {
+			params.auth.Nonce = big.NewInt(params.auth.Nonce.Int64() + 1)
+			return makeAddFeedbackTx(ctx, params)
+		}
+
+		return errors.Wrap(err, "failed to add feedback")
+	}
+
+	params.request.Status = data.RequestsStatusInProgress
+	err = params.requestsQ.Update(params.request)
+	if err != nil {
+		return errors.Wrap(err, "failed to update request status")
+	}
+
+	helpers.WaitForTransactionMined(
+		ctx,
+		clients[params.network],
+		transaction,
+		params.logger,
+		params.request,
+		params.requestsQ,
+	)
+
+	return nil
+}
